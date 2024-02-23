1021b4af0a26632aa08df8721860a75794ef0bef
muzakhan <muzammil.it5690@gmail.com>
2024-02-23 21:30:10 +0530

    fix

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map config=[:]){
     def gitTool=tool 'git'
     echo config.url
-    currentBuild.id=currentBuild.number + currentBuild.projectName
+    currentBuild.displayName=currentBuild.number + currentBuild.projectName
      git branch: 'master', url: "${config.url}"
     
 }
